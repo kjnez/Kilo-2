5276b143816b57aea358e8cde07608cf05d66ebf
Yingnan Cui <yingn.cui@gmail.com>
2022-12-17 17:10:05 -0500

    abuf-append.

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -107,6 +108,21 @@ struct abuf {
 };
 
 #define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new, s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+  free(ab->b);
+}
 
 /* output */
 void editorDrawRows()
