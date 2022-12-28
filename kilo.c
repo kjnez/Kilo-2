fda1d3b56ddd28f47cc3fbb308d95041ef0c586f
Yingnan Cui <yingn.cui@gmail.com>
2022-12-28 00:03:18 -0500

    move-cursor

--- kilo.c
+++ kilo.c
@@ -174,6 +174,24 @@ void editorRefreshScreen()
 }
 
 /* input */
+void editorMoveCursor(char key)
+{
+  switch (key) {
+  case 'a':
+    E.cx--;
+    break;
+  case 'd':
+    E.cx++;
+    break;
+  case 'w':
+    E.cy--;
+    break;
+  case 's':
+    E.cy++;
+    break;
+  }
+}
+
 void editorProcessKeypress()
 {
   char c = editorReadKey();
@@ -184,6 +202,13 @@ void editorProcessKeypress()
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+  case 'w':
+  case 's':
+  case 'a':
+  case 'd':
+    editorMoveCursor(c);
+    break;
   }
 }
 
