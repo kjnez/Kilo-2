5c83477ec30ff410b7d076bd18fb682d11d0d5f6
Yingnan Cui <yingn.cui@gmail.com>
2022-12-28 22:10:43 -0500

    detect-page-up-down

--- kilo.c
+++ kilo.c
@@ -18,7 +18,9 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /* Data */
@@ -79,14 +81,23 @@ int editorReadKey()
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
     if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+	if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+	if (seq[2] == '~') {
+	  switch (seq[1]) {
+	  case '5': return PAGE_UP;
+	  case '6': return PAGE_DOWN;
+	  }
+	}
+      } else {
       switch (seq[1]) {
       case 'A': return ARROW_UP;
       case 'B': return ARROW_DOWN;
       case 'C': return ARROW_RIGHT;
       case 'D': return ARROW_LEFT;
       }
+      }
     }
-
     return '\x1b';
   } else {
     return c;
