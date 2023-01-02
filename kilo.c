89793fbee3eb436ba0f0d1ee455379955977ad04
Yingnan Cui <yingn.cui@gmail.com>
2023-01-01 22:55:25 -0500

    hello-world

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
@@ -28,11 +29,17 @@ enum editorKey {
 
 /* Data */
 /* struct termios orig_termios; */
+typedef struct {
+  int size;
+  char *chars;
+} erow;
 
 struct editorConfig {
   int cx, cy;
   int screenrows;
   int screencols;
+  int numrows;
+  erow row;
   struct termios orig_termios;
 } E;
 
@@ -153,6 +160,19 @@ int getWindowSize(int *rows, int *cols)
     *rows = ws.ws_row;
     return 0;
   }
+}
+
+/* file i/o */
+void editorOpen()
+{
+  char *line = "Hello, world!";
+  ssize_t linelen = 13;
+
+  E.row.size = linelen;
+  E.row.chars = malloc(linelen + 1);
+  memcpy(E.row.chars, line, linelen);
+  E.row.chars[linelen] = '\0';
+  E.numrows = 1;
 }
 
 /* append buffer */
@@ -289,7 +309,8 @@ void initEditor()
 {
   E.cx = 0;
   E.cy = 0;
-  
+  E.numrows = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
@@ -298,6 +319,7 @@ int main()
 {
   enableRawMode();
   initEditor();
+  editorOpen();
 
   while (1) {
     editorRefreshScreen();
