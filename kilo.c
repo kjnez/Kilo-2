c1786648e2b0bfe5a3c27fe285e84024254a955a
Yingnan Cui <yingn.cui@gmail.com>
2023-01-15 23:39:42 -0500

    scroll-limits

--- kilo.c
+++ kilo.c
@@ -41,6 +41,7 @@ typedef struct {
 struct editorConfig {
   int cx, cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -229,6 +230,10 @@ void editorScroll()
     E.rowoff = E.cy;
   if (E.cy >= E.rowoff + E.screenrows)
     E.rowoff = E.cy - E.screenrows + 1;
+  if (E.cx < E.coloff)
+    E.coloff = E.cx;
+  if (E.cx >= E.coloff + E.screencols)
+    E.coloff = E.cx - E.screencols + 1;
 }
 
 void editorDrawRows(struct abuf *ab)
@@ -253,9 +258,10 @@ void editorDrawRows(struct abuf *ab)
 	abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[filerow].chars, len);
+      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
     abAppend(ab, "\x1b[K", 3);
     
@@ -276,7 +282,7 @@ void editorRefreshScreen()
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -288,13 +294,15 @@ void editorRefreshScreen()
 /* input */
 void editorMoveCursor(int key)
 {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
   case ARROW_LEFT:
     if (E.cx != 0)
       E.cx--;
     break;
   case ARROW_RIGHT:
-    if (E.cx != E.screencols - 1)
+    if (row && E.cx < row->size)
       E.cx++;
     break;
   case ARROW_UP:
@@ -350,6 +358,7 @@ void initEditor()
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
