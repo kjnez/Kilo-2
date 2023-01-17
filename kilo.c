a0744529c17278d4d2ba4756f9a6ea52215287c1
Yingnan Cui <yingn.cui@gmail.com>
2023-01-16 23:06:08 -0500

    render

--- kilo.c
+++ kilo.c
@@ -35,7 +35,9 @@ enum editorKey {
 /* struct termios orig_termios; */
 typedef struct {
   int size;
+  int rsize;
   char *chars;
+  char *render;
 } erow;
 
 struct editorConfig {
@@ -177,6 +179,10 @@ void editorAppendRow(char *s, size_t len)
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+
   E.numrows++;
 }
 
