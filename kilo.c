42da5abea24e7d4a4140e6709e8983fe1af3fd7c
Yingnan Cui <yingn.cui@gmail.com>
2023-01-28 17:11:24 -0500

    append-to-insert and use-insert-row

--- kilo.c
+++ kilo.c
@@ -220,10 +220,13 @@ void editorUpdateRow(erow *row)
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
+  if (at < 0 || at > E.numrows) return;
+  
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
-  int at = E.numrows;
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -286,7 +289,7 @@ void editorRowDelChar(erow *row, int at)
 void editorInsertChar(int c)
 {
   if (E.cy == E.numrows)
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
 }
@@ -344,7 +347,7 @@ void editorOpen(char *filename)
     while (linelen > 0 && (line[linelen - 1] == '\n' ||
 			   line[linelen - 1] == '\r'))
       linelen--;
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
