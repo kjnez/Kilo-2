eea0eb98a8cd8e36240c5e2608e31b96d9d42fd2
Yingnan Cui <yingn.cui@gmail.com>
2023-01-28 12:38:10 -0500

    rows-to-string

--- kilo.c
+++ kilo.c
@@ -250,6 +250,25 @@ void editorInsertChar(int c)
 }
 
 /* file i/o */
+char *editorRowsToString(int *buflen)
+{
+  int totlen = 0;
+  int j;
+  for (j = 0; j < E.numrows; j++)
+    totlen += E.row[j].size + 1;
+  *buflen = totlen;
+
+  char *buf = malloc(totlen);
+  char *p = buf;
+  for (j = 0; j < E.numrows; j++) {
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
+  return buf;
+}
+
 void editorOpen(char *filename)
 {
   free(E.filename);
@@ -455,7 +474,7 @@ void editorProcessKeypress()
   case '\r':
     /* TODO */
     break;
-    
+
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
