217e8fdc5a5086fecab9f1f6ce565abb89b593a2
Yingnan Cui <yingn.cui@gmail.com>
2023-01-27 22:16:54 -0500

    rx

--- kilo.c
+++ kilo.c
@@ -17,6 +17,8 @@
 
 /* defines */
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
@@ -42,6 +44,7 @@ typedef struct {
 
 struct editorConfig {
   int cx, cy;
+  int rx;
   int rowoff;
   int coloff;
   int screenrows;
@@ -173,13 +176,22 @@ int getWindowSize(int *rows, int *cols)
 /* row operations */
 void editorUpdateRow(erow *row)
 {
-  free(row->render);
-  row->render = malloc(row->size + 1);
-
+  int tabs = 0;
   int j;
-  int idx = 0;
   for (j = 0; j < row->size; j++)
-    row->render[idx++] = row->chars[j];
+    if (row->chars[j] == '\t') tabs++;
+
+  free(row->render);
+  row->render = malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1);
+
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+    } else
+      row->render[idx++] = row->chars[j];
+  }
   row->render[idx] = '\0';
   row->rsize = idx;
 }
@@ -390,6 +402,7 @@ void initEditor()
 {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
