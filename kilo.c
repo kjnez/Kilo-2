669e10129ae54defab49041240e0fc0eb805c821
Yingnan Cui <yingn.cui@gmail.com>
2023-01-29 16:39:23 -0500

    decimal-point

--- kilo.c
+++ kilo.c
@@ -211,7 +211,8 @@ void editorUpdateSyntax(erow *row)
   while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
-    if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) {
+    if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
+	(c == '.' && prev_hl == HL_NUMBER)) {
       row->hl[i++] = HL_NUMBER;
       prev_sep = 0;
       continue;
