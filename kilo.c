f3ea42981ecec85eef7dfbe002872b141dedfb10
Yingnan Cui <yingn.cui@gmail.com>
2023-01-28 19:05:44 -0500

    search-arrows-help

--- kilo.c
+++ kilo.c
@@ -436,18 +436,18 @@ void editorFindCallback(char *query, int key)
   }
 
   if (last_match == -1) direction = 1;
-  int current = last_match;
+  int current_row = last_match;
   
   int i;
   for (i = 0; i < E.numrows; i++) {
-    current += direction;
-    if (current == -1) current = E.numrows - 1;
-    else if (current == E.numrows) current = 0;
-    erow *row = &E.row[current];
+    current_row += direction;
+    if (current_row == -1) current_row = E.numrows - 1;
+    else if (current_row == E.numrows) current_row = 0;
+    erow *row = &E.row[current_row];
     char *match = strstr(row->render, query);
     if (match) {
-      last_match = current;
-      E.cy = current;
+      last_match = current_row;
+      E.cy = current_row;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
       break;
@@ -461,7 +461,7 @@ void editorFind() {
   int saved_coloff = E.coloff;
   int saved_rowoff = E.rowoff;
   
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+  char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
   if (query)
     free(query);
   else {
