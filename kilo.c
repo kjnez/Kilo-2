1dfd22f25e652322e692aea73f44cca38a25cc96
Yingnan Cui <yingn.cui@gmail.com>
2023-01-29 16:21:05 -0500

    restore-hl

--- kilo.c
+++ kilo.c
@@ -454,6 +454,15 @@ void editorFindCallback(char *query, int key)
 {
   static int last_match = -1;
   static int direction = 1;
+
+  static int saved_hl_line;
+  static char *saved_hl = NULL;
+
+  if (saved_hl) {
+    memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
   
   if (key == '\r' || key == '\x1b') {
     last_match = -1;
@@ -469,20 +478,24 @@ void editorFindCallback(char *query, int key)
   }
 
   if (last_match == -1) direction = 1;
-  int current_row = last_match;
+  int current_line = last_match;
   
   int i;
   for (i = 0; i < E.numrows; i++) {
-    current_row += direction;
-    if (current_row == -1) current_row = E.numrows - 1;
-    else if (current_row == E.numrows) current_row = 0;
-    erow *row = &E.row[current_row];
+    current_line += direction;
+    if (current_line == -1) current_line = E.numrows - 1;
+    else if (current_line == E.numrows) current_line = 0;
+    erow *row = &E.row[current_line];
     char *match = strstr(row->render, query);
     if (match) {
-      last_match = current_row;
-      E.cy = current_row;
+      last_match = current_line;
+      E.cy = current_line;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      saved_hl_line = current_line;
+      saved_hl = malloc(row->rsize);
+      memcpy(saved_hl, row->hl, row->rsize);
 
       memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
