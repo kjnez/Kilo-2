a75c604a01c5757fa6720729f91c235f1c6357c4
Yingnan Cui <yingn.cui@gmail.com>
2023-01-29 16:26:24 -0500

    syntax-while

--- kilo.c
+++ kilo.c
@@ -200,10 +200,11 @@ void editorUpdateSyntax(erow *row)
   row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
 
-  int i;
-  for (i = 0; i < row->rsize; i++) {
-    if (isdigit(row->render[i]))
-      row->hl[i] = HL_NUMBER;
+  int i = 0;
+  while (i < row->rsize) {
+    char c = row->render[i];
+    if (isdigit(c))
+      row->hl[i++] = HL_NUMBER;
   }
 }
 
