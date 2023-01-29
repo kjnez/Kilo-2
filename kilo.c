12b35e7793146f9ad10b0b715c306861827615ed
Yingnan Cui <yingn.cui@gmail.com>
2023-01-28 19:21:20 -0500

    syntax-refactoring

--- kilo.c
+++ kilo.c
@@ -45,6 +45,7 @@ typedef struct {
   int rsize;
   char *chars;
   char *render;
+  unsigned char *hl;
 } erow;
 
 struct editorConfig {
@@ -250,6 +251,7 @@ void editorInsertRow(int at, char *s, size_t len)
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
@@ -260,6 +262,7 @@ void editorFreeRow(erow *row)
 {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 
 void editorDelRow(int at)
