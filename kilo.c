b3001f6f90ea40359345032107e3d3c0c4d6d842
Yingnan Cui <yingn.cui@gmail.com>
2023-01-29 19:52:33 -0500

    syntax-keywords

--- kilo.c
+++ kilo.c
@@ -245,6 +245,8 @@ void editorUpdateSyntax(erow *row)
 
   if (E.syntax == NULL) return;
 
+  char **keywords = E.syntax->keywords;
+
   char *scs = E.syntax->singleline_comment_start; /* singleline comment start */
   int scs_len = scs ? strlen(scs) : 0;
   
@@ -287,6 +289,26 @@ void editorUpdateSyntax(erow *row)
       if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
 	  (c == '.' && prev_hl == HL_NUMBER)) {
 	row->hl[i++] = HL_NUMBER;
+	prev_sep = 0;
+	continue;
+      }
+    }
+
+    if (prev_sep) {
+      int j;
+      for (j = 0; keywords[j]; j++) {
+	int klen = strlen(keywords[j]);
+	int kw2 = keywords[j][klen - 1] == '|';
+	if (kw2) klen--;
+
+	if (!strncmp(&row->render[i], keywords[j], klen) &&
+	    is_separator(row->render[i + klen])) {
+	  memset(&row->hl[i], kw2 ? HL_KEYWORD2 : HL_KEYWORD1, klen);
+	  i += klen;
+	  break;
+	}
+      }
+      if (keywords[j] != NULL) {
 	prev_sep = 0;
 	continue;
       }
